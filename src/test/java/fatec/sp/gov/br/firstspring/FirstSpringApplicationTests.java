4db9b42d340e5f1ae65099c6d6cba393edbe3c57
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-08-07 14:01:53 -0300

    Fisrt commit

---
+++ src/test/java/fatec/sp/gov/br/firstspring/FirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.sp.gov.br.firstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
